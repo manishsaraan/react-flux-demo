ee53c7af3eb64922b6e34d6c1963f26b7a785c43
manishsaraan <manish021js@gmail.com>
2017-11-26 19:13:01 +0530

    adding new author component

--- src/components/authors/authorPage.js
+++ src/components/authors/authorPage.js
@@ -2,6 +2,8 @@
 var React = require('react');
 var AuthorApi = require('../../api/authorApi');
 var AuthorList = require('./authorList');
+var Link = require('react-router').Link;
+
 var  Authors = React.createClass({
 	  getInitialState: function(){
 	  	 return {
@@ -20,6 +22,7 @@ var  Authors = React.createClass({
         return(
                <div>
                    <h1>Authors List</h1>  
+                   <Link to="addAuthor" className="btn btn-default">Add Author</Link>
                    <AuthorList authors={this.state.authors} />               
                </div>
       	 	);
---
+++ src/components/authors/manageAuthorPage.js
@@ -0,0 +1,13 @@
+'use strict';
+var React = require('react');
+var MangeAuthorPage = React.createClass({
+	 render(){
+	 	return(
+                <div>
+                   <h1>Add Author</h1>
+                </div>
+	 		);
+	 }
+});
+
+module.exports = MangeAuthorPage;
