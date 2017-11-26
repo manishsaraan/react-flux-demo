a324ec397096eb754a5864fd3c90b5a6738886f5
manishsaraan <manish021js@gmail.com>
2017-11-26 12:57:04 +0530

    initals react component added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,32 @@
 $ = jQuery = require('jquery');
-var misn = 1;
-var App = console.log('Hello From Browserify');
-module.exports = App;
+var React = require('react');
+var Home = require('./components/homePage');
+var About = require('./components/about/aboutPage');
+
+(function(window){
+'use strict';
+
+var App = React.createClass({
+	render(){
+		var Child;
+		switch(this.props.route){
+			case  'about' : Child  = About; break;
+			default : Child  = Home; 
+		}
+
+		return (
+                <div>
+                 <Child/>
+                </div>
+			);
+	}
+});
+
+function render(){
+	var route = window.location.hash.substr(1);
+	React.render(<App route= {route} />, document.getElementById('app'));
+
+}
+window.addEventListener('hashChange', render);
+render();
+})(window);
